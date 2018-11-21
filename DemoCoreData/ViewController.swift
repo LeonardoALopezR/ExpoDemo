a507ec721cd07e7941acb00452a300cd6b85f10c
Leonardo López Romero <leo_fan129albert@hotmail.com>
2018-11-21 12:37:11 -0600

    Initial Commit

---
+++ DemoCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoCoreData
+//
+//  Created by Leonardo on 11/21/18.
+//  Copyright © 2018 Leonardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
